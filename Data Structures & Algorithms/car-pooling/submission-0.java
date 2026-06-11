325928d70fa7af9e65b9d9fda2ff7e3f15c3cac6
abhishekyalla0709 <112754517+abhishekyalla0709@users.noreply.github.com>
2026-06-11 11:54:59 +0530

    Add: car-pooling - submission-0

---
+++ Data Structures & Algorithms/car-pooling/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        Arrays.sort(trips, (a,b) -> Integer.compare(a[1],b[1]));
+        int cap = 0;
+        int dist = 0;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
+        for(int i = 0; i < trips.length; i++){
+            cap+=trips[i][0];
+            dist=trips[i][1];
+            while(!minHeap.isEmpty() && minHeap.peek()[2]<=dist){
+                cap-=minHeap.poll()[0];
+            }
+            if(cap > capacity){
+                return false;
+            }
+            minHeap.offer(trips[i]);
+        }
+        return true;
+    }
+}
